8bd79f199c0f851cd26ee2ff0546ec9a17dfde0a
WenzhengZhang <wenzheng.zhang@rutgers.edu>
2023-05-03 10:12:09 -0400

    zeshel scripts

--- scripts/zeshel/shells/setup_data.sh
+++ scripts/zeshel/shells/setup_data.sh
@@ -20,14 +20,13 @@ if [ ! -d $ORIG_DIR ]; then
   echo "please download zeshel data from https://github.com/lajanugen/zeshel and put it in $ORIG_DIR"
 else
   cd $CODE_DIR
-  echo "preprocess data"
-  python $CODE_DIR/scripts/zeshel/preprocess_data.py --input_dir $ORIG_DIR \
-    --output_dir $RAW_DIR \
-    --max_len 180
+#  echo "preprocess data"
+#  python $CODE_DIR/scripts/zeshel/preprocess_data.py --input_dir $ORIG_DIR \
+#    --output_dir $RAW_DIR \
+#    --max_len 180
   echo "build train "
   python $CODE_DIR/scripts/zeshel/build_train.py \
         --tokenizer_name $PLM_DIR/t5-base-scaled  \
-        --negative_file $ORIG_DIR/tfidf_candidates/train.json  \
         --qrels $RAW_DIR/train.qrel.tsv  \
         --queries $RAW_DIR/train.query.txt  \
         --collection $RAW_DIR/psg_corpus_train.tsv  \
