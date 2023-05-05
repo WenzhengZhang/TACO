24a4c39583c3ae9554def3f5e2789ab8fb47ef0c
WenzhengZhang <wenzheng.zhang@rutgers.edu>
2023-05-05 17:10:45 -0400

    less hard negatives

--- src/taco/dataset/train_dataset.py
+++ src/taco/dataset/train_dataset.py
@@ -204,11 +204,8 @@ class DRTrainDataset(TrainDatasetBase):
                 rand_negatives = example['random_negatives'][:num_rand]
             if len(group_negatives) < num_hard:
                 if hashed_seed is not None:
-                    try:
-                        hard_negatives = random.choices(group_negatives,
+                    hard_negatives = random.choices(group_negatives,
                                                     k=num_hard)
-                    except IndexError:
-                        print(self.task_name)
                 else:
                     hard_negatives = [x for x in group_negatives]
                     hard_negatives = hard_negatives * 2
