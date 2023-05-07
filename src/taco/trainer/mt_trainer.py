7392cb6925b60a1df29312fd55e509a1de9261dd
WenzhengZhang <wenzheng.zhang@rutgers.edu>
2023-05-07 15:37:42 -0400

    beir nq style

--- src/taco/trainer/mt_trainer.py
+++ src/taco/trainer/mt_trainer.py
@@ -442,7 +442,7 @@ class MTDenseTrainer(DenseTrainer):
             else:
                 taco_warmup_steps = int(self.state.max_steps *
                                         self.args.warmup_ratio)
-                logger.info(f'warmup steps {taco_warmup_steps}')
+                # logger.info(f'warmup steps {taco_warmup_steps}')
                 # with torch.no_grad():
                 #     # T x p
                 ipt = self.get_ipt(grads)
