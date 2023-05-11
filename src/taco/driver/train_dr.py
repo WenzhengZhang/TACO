229203e9385d80b5b0315838266c600d6ca6b5cc
WenzhengZhang <wenzheng.zhang@rutgers.edu>
2023-05-11 09:15:44 -0400

    ance mt

--- src/taco/driver/train_dr.py
+++ src/taco/driver/train_dr.py
@@ -39,7 +39,8 @@ def main():
     # Lr decay while iterative training with hard negative
     if not training_args.hard_negative_mining:
         if model_args.total_iter_num is not None:
-            decay_rate = training_args.learning_rate / model_args.total_iter_num
+            decay_rate = training_args.learning_rate / (
+                    model_args.total_iter_num + 1)
         else:
             decay_rate = model_args.decay_rate
         training_args.learning_rate -= model_args.iter_num * decay_rate
